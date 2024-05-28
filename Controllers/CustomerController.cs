f09ca2a8dee66eb3b416447f942ec72d456b3da0
Derpy113 <0210joma@gapp.uddevalla.se>
2024-05-28 19:14:21 +0200

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ajax2.Models;
+using Microsoft.AspNetCore.Components.Forms.Mapping;
+
+namespace ajax2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        Customer customer;
+        List<Customer> customers;
+
+        public CustomerController()
+        {
+            //Get some data - usually comes form a database
+
+            //In this case we will hard-code the data
+            customers = new List<Customer>();
+            customers.Add(new Customer(0, "Sherry", 11));
+            customers.Add(new Customer(1, "Adam", 12));
+            customers.Add(new Customer(2, "Kevin", 19));
+            customers.Add(new Customer(3, "Candice", 26));
+            customers.Add(new Customer(4, "Name", 36));
+            customers.Add(new Customer(5, "Shell", 39));
+            customers.Add(new Customer(6, "Sherry 2", 50));
+        }
+
+        // GET: Customer 
+        public IActionResult Customer(int customerNumber)
+        {
+            List<string> customers = new List<string>();
+            customers.Add("Adam");
+            customers.Add("Beatrice");
+            customers.Add("Charlie");
+            (List<string>, Customer) tulpa = (customers, this.customers[customerNumber]);
+                
+            return View(tulpa);
+        }
+
+        public IActionResult OnSelectCustomer(string Customers)
+        {
+            int i;
+            if (Customers == "Adam")
+            {
+                i = 1;
+            }
+            else if(Customers == "Beatrice")
+            {
+                i = 2;
+            }
+            else
+            {
+                i = 3;
+            }
+
+            return PartialView("_CustomersDetails", customers[i]);
+            //return RedirectToAction("Customer", new { customerNumber = Int32.Parse(customerNumber) });
+        }
+
+        public IActionResult Face(string[] color, string[] brand, string[] PurposeFor, string sortOrder)
+        {
+            //return View(filtercars.ToList());
+            return View();
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace ajax2.Models
+{
+    public class Customer
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Customer(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+    }
+}
